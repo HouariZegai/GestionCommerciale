be835377a496486a411d3858ff9785bd3a9df6f2
HouariZEGAI <houarizegai14@gmail.com>
2018-11-30 16:43:42 +0100

    Adding Login GUI

---
+++ src/com/houarizegai/gestioncommercial/java/Launcher.java
@@ -0,0 +1,28 @@
+package com.houarizegai.gestioncommercial.java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage stage) {
+        Parent root = null;
+        try { // load the FXML file
+            root = FXMLLoader.load(getClass().getResource("/com/houarizegai/gestioncommercial/resources/views/Login.fxml"));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        stage.setScene(new Scene(root)); // Add the GUI to scene and the scene to stage (GUI > Scene > Stage)
+
+        // Adding icon of App
+        stage.getIcons().add(new Image("/com/houarizegai/gestioncommercial/resources/images/gc-logo-48px.png"));
+        stage.setTitle("Gestion Commercial"); // Change the title of app
+        stage.show(); // make stage visible
+    }
+}
