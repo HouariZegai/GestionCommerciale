e5a2b46cd3c07da1f913d203b3dddf24c015c011
HouariZEGAI <houarizegai14@gmail.com>
2018-12-25 18:23:18 +0100

    Making Select Produit & Making actions for product table (crud)

--- src/com/houarizegai/gestioncommercial/java/controllers/FactureFournisseurController.java
+++ src/com/houarizegai/gestioncommercial/java/controllers/FactureFournisseurController.java
@@ -1,6 +1,7 @@
 package com.houarizegai.gestioncommercial.java.controllers;
 
 import com.houarizegai.gestioncommercial.java.database.dao.ProduitDao;
+import com.houarizegai.gestioncommercial.java.database.dao.TVADao;
 import com.houarizegai.gestioncommercial.java.database.models.Fournisseur;
 import com.houarizegai.gestioncommercial.java.database.models.Produit;
 import com.jfoenix.controls.*;
@@ -58,7 +59,23 @@ public class FactureFournisseurController implements Initializable {
 
     private JFXTreeTableColumn<TableProduit, String> colRef, colDesignation, colQte, colPU, colMHT, colTVA, colMTTC;
 
+    // Data of Table
+    private ObservableList<TableProduit> listProduits;
+
     /* End Product Table */
+
+    /* Start Product Form infos */
+
+    @FXML
+    private JFXTextField fieldQte;
+
+    public static Produit selectedProduit;
+
+    public static JFXDialog dialogSelectProduit;
+
+    private VBox selectProduitView;
+
+    /* End Product Form infos */
 
     // Payment Mode Number
     @FXML
@@ -70,6 +87,7 @@ public class FactureFournisseurController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         // init Date of facture
         pickerDate.setValue(LocalDate.now());
 
@@ -82,6 +100,12 @@ public class FactureFournisseurController implements Initializable {
 
         // Initialize table product
         initProduitTable();
+        initDataOfProduitTable();
+
+        // on Change Select
+        tableProduit.setOnMouseClicked((e -> {
+            fieldQte.setText(colQte.getCellData(tableProduit.getSelectionModel().getSelectedIndex()));
+        }));
     }
 
     /* Start Table Produit */
@@ -108,37 +132,47 @@ public class FactureFournisseurController implements Initializable {
 
     private void initProduitTable() { // This function initialize the table by colunms
         colRef = new JFXTreeTableColumn<>("Reference");
-        colRef.setPrefWidth(180);
+        colRef.setPrefWidth(80);
         colRef.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().ref);
 
         colDesignation = new JFXTreeTableColumn<>("Désignation");
-        colDesignation.setPrefWidth(250);
+        colDesignation.setPrefWidth(120);
         colDesignation.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().designation);
 
 
         colQte = new JFXTreeTableColumn<>("Qte");
-        colQte.setPrefWidth(150);
+        colQte.setPrefWidth(70);
         colQte.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().qte);
 
-        colPU = new JFXTreeTableColumn<>("Prix Unitaire");
-        colPU.setPrefWidth(150);
+        colPU = new JFXTreeTableColumn<>("P.U");
+        colPU.setPrefWidth(100);
         colPU.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().pu);
 
-        colMHT = new JFXTreeTableColumn<>("Montant HT");
-        colMHT.setPrefWidth(150);
+        colMHT = new JFXTreeTableColumn<>("MHT");
+        colMHT.setPrefWidth(100);
         colMHT.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().mht);
 
         colTVA = new JFXTreeTableColumn<>("TVA %");
-        colTVA.setPrefWidth(100);
+        colTVA.setPrefWidth(80);
         colTVA.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().tva);
 
-        colMTTC = new JFXTreeTableColumn<>("Montant TTC");
-        colMTTC.setPrefWidth(160);
+        colMTTC = new JFXTreeTableColumn<>("MTTC");
+        colMTTC.setPrefWidth(100);
         colMTTC.setCellValueFactory((TreeTableColumn.CellDataFeatures<TableProduit, String> param) -> param.getValue().getValue().mttc);
 
         // Add columns to table
         tableProduit.getColumns().addAll(colRef, colDesignation, colQte, colPU, colMHT, colTVA, colMTTC);
         tableProduit.setShowRoot(false);
+    }
+
+    private void initDataOfProduitTable() {
+        listProduits = FXCollections.observableArrayList();
+        final TreeItem<TableProduit> treeItem = new RecursiveTreeItem<>(listProduits, RecursiveTreeObject::getChildren);
+        try {
+            tableProduit.setRoot(treeItem);
+        } catch (Exception ex) {
+            System.out.println("Error catched !");
+        }
     }
 
     /* End Table Produit */
@@ -167,17 +201,59 @@ public class FactureFournisseurController implements Initializable {
 
     @FXML
     private void onAdd() {
+        // Load Select Produit View
+        try {
+            selectProduitView = FXMLLoader.load(getClass().getResource("/com/houarizegai/gestioncommercial/resources/views/forms/facture_fournisseur/SelectProduit.fxml"));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
 
+        dialogSelectProduit = new JFXDialog(root, selectProduitView, JFXDialog.DialogTransition.CENTER);
+        dialogSelectProduit.setOnDialogClosed(e -> { // if i close dialog: make the fournisseur selected
+            if(selectedProduit == null)
+                return;
+            listProduits.add(new TableProduit(selectedProduit.getReference(),
+                    selectedProduit.getLibProd(),
+                    0,
+                    selectedProduit.getPrixHt(),
+                    0.0,
+                    selectedProduit.getTauxTva(),
+                    0.0));
+        });
+        dialogSelectProduit.show();
     }
 
     @FXML
-    private void onDelete() {
+    private void onDelete() { // delete product
+        if(tableProduit.getSelectionModel().getSelectedItem() == null)
+            return;
 
+        tableProduit.getRoot().getChildren().remove(tableProduit.getSelectionModel().getSelectedIndex());
     }
 
     @FXML
-    private void onEdit() {
+    private void onEdit() { // Edit Qte of product
+        if(fieldQte.getText() == null || !fieldQte.getText().trim().matches("[0-9]{1,3}")) {
+            return;
+        }
+        if(tableProduit.getSelectionModel().getSelectedItem() == null)
+            return;
 
+        System.out.println("passed !");
+
+        int index = tableProduit.getSelectionModel().getSelectedIndex();
+        TableProduit produit = listProduits.get(index);
+        produit.qte = new SimpleStringProperty(fieldQte.getText().trim());
+
+        listProduits.set(index, produit);
+        final TreeItem<TableProduit> treeItem = new RecursiveTreeItem<>(listProduits, RecursiveTreeObject::getChildren);
+        try {
+            tableProduit.setRoot(treeItem);
+        } catch (Exception ex) {
+            System.out.println("Error catched !");
+        }
+
+        fieldQte.setText(null);
     }
 
     @FXML
